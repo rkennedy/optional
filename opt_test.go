58673e3586fdf7523934eca65b9d098b605e67b2
Rob Kennedy <rob@sweetkennedy.net>
2022-11-11 23:18:12 -0600

    Arrange more test cases into subtests

--- opt_test.go
+++ opt_test.go
@@ -10,152 +10,154 @@ import (
 	opt "github.com/rkennedy/optional/testing"
 )
 
-func TestCreateEmpty(t *testing.T) {
+func TestCreate(t *testing.T) {
 	t.Parallel()
-	g := NewWithT(t)
+	t.Run("empty", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
 
-	var o Value[string]
-	g.Expect(o.Present()).To(BeFalse())
-}
+		var o Value[string]
+		g.Expect(o.Present()).To(BeFalse())
+	})
+	t.Run("full", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
 
-func TestCreateFull(t *testing.T) {
-	t.Parallel()
-	g := NewWithT(t)
-
-	o := New("foo")
-	g.Expect(o.Present()).To(BeTrue())
-}
-
-func TestGetEmpty(t *testing.T) {
-	t.Parallel()
-	g := NewWithT(t)
-
-	var o Value[int]
-	g.Expect(o.Get()).Error().To(MatchError(ErrEmpty))
-}
-
-func TestGetFull(t *testing.T) {
-	t.Parallel()
-	g := NewWithT(t)
-
-	o := New(9)
-	g.Expect(o.Get()).To(Equal(9))
-}
-
-func TestMustGetEmpty(t *testing.T) {
-	t.Parallel()
-	g := NewWithT(t)
-
-	var o Value[float64]
-	g.Expect(func() { o.MustGet() }).To(PanicWith(ErrEmpty))
-}
-
-func TestMustGetFull(t *testing.T) {
-	t.Parallel()
-	g := NewWithT(t)
-
-	o := New(9.5)
-	g.Expect(o.MustGet()).To(Equal(9.5))
-}
-
-func TestIfEmpty(t *testing.T) {
-	t.Parallel()
-
-	var o Value[any]
-	o.If(func(a any) {
-		t.Fail()
+		o := New("foo")
+		g.Expect(o.Present()).To(BeTrue())
 	})
 }
 
-func TestIfFull(t *testing.T) {
+func TestGet(t *testing.T) {
 	t.Parallel()
-	g := NewWithT(t)
+	t.Run("empty", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
 
-	o := New[any]('a')
-	called := false
-	o.If(func(a any) {
-		g.Expect(a).To(Equal('a'))
-		called = true
+		var o Value[int]
+		g.Expect(o.Get()).Error().To(MatchError(ErrEmpty))
 	})
-	g.Expect(called).To(BeTrue())
+	t.Run("full", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
+
+		o := New(9)
+		g.Expect(o.Get()).To(Equal(9))
+	})
 }
 
-func TestMarshalEmpty(t *testing.T) {
+func TestMustGet(t *testing.T) {
 	t.Parallel()
-	g := NewWithT(t)
+	t.Run("empty", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
 
-	var o Value[bool]
-	b, err := o.MarshalJSON()
-	g.Expect(string(b), err).To(Equal("null"))
+		var o Value[float64]
+		g.Expect(func() { o.MustGet() }).To(PanicWith(ErrEmpty))
+	})
+	t.Run("full", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
+
+		o := New(9.5)
+		g.Expect(o.MustGet()).To(Equal(9.5))
+	})
 }
 
-func TestMarshalFull(t *testing.T) {
+func TestIf(t *testing.T) {
 	t.Parallel()
+	t.Run("empty", func(t *testing.T) {
+		t.Parallel()
 
-	cases := []struct {
-		Input   any
-		Marshal string
-	}{
-		{4, `4`},
-		{true, `true`},
-		{nil, `null`},
-		{"foo", `"foo"`},
-	}
-	for _, value := range cases {
-		input, marshal := value.Input, value.Marshal
-		t.Run(fmt.Sprintf("%v", input), func(t *testing.T) {
-			t.Parallel()
-			g := NewWithT(t)
-			o := New(input)
-			b, err := o.MarshalJSON()
-			g.Expect(string(b), err).To(Equal(marshal))
+		var o Value[any]
+		o.If(func(a any) {
+			t.Fail()
 		})
-	}
+	})
+	t.Run("full", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
+
+		o := New[any]('a')
+		called := false
+		o.If(func(a any) {
+			g.Expect(a).To(Equal('a'))
+			called = true
+		})
+		g.Expect(called).To(BeTrue())
+	})
+}
+
+func TestMarshal(t *testing.T) {
+	t.Parallel()
+	t.Run("empty", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
+
+		var o Value[bool]
+		b, err := o.MarshalJSON()
+		g.Expect(string(b), err).To(Equal("null"))
+	})
+	t.Run("full", func(t *testing.T) {
+		t.Parallel()
+
+		cases := []struct {
+			Input   any
+			Marshal string
+		}{
+			{4, `4`},
+			{true, `true`},
+			{nil, `null`},
+			{"foo", `"foo"`},
+		}
+		for _, value := range cases {
+			input, marshal := value.Input, value.Marshal
+			t.Run(fmt.Sprintf("%v", input), func(t *testing.T) {
+				t.Parallel()
+				g := NewWithT(t)
+				o := New(input)
+				b, err := o.MarshalJSON()
+				g.Expect(string(b), err).To(Equal(marshal))
+			})
+		}
+	})
 }
 
 func TestOrElseEmpty(t *testing.T) {
 	t.Parallel()
-	g := NewWithT(t)
+	t.Run("empty", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
 
-	var o Value[rune]
-	g.Expect(o.OrElse('r')).To(Equal('r'))
+		var o Value[rune]
+		g.Expect(o.OrElse('r')).To(Equal('r'))
+	})
+	t.Run("full", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
+
+		o := New('q')
+		g.Expect(o.OrElse('r')).To(Equal('q'))
+	})
 }
 
-func TestOrElseFull(t *testing.T) {
+func TestUnmarshal(t *testing.T) {
 	t.Parallel()
-	g := NewWithT(t)
+	t.Run("null", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
 
-	o := New('q')
-	g.Expect(o.OrElse('r')).To(Equal('q'))
-}
-
-func TestUnmarshalNull(t *testing.T) {
-	t.Parallel()
-	g := NewWithT(t)
-
-	var o Value[int]
-	g.Expect(o.UnmarshalJSON([]byte(`null`))).To(Succeed())
-	g.Expect(o.Present()).To(BeFalse())
-}
-
-type NonerrorError struct {
-	Value any
-}
-
-func (e NonerrorError) Error() string {
-	return fmt.Sprintf("%#v is not an error", e.Value)
-}
-
-func TestUnmarshalInt(t *testing.T) {
-	t.Parallel()
-
+		var o Value[int]
+		g.Expect(o.UnmarshalJSON([]byte(`null`))).To(Succeed())
+		g.Expect(o).To(opt.BeEmpty[int]())
+	})
 	t.Run("right type", func(t *testing.T) {
 		t.Parallel()
 		g := NewWithT(t)
 
 		var o Value[int]
 		g.Expect(o.UnmarshalJSON([]byte(`1`))).To(Succeed())
-		g.Expect(o.Get()).To(Equal(1))
+		g.Expect(o).To(opt.HaveValue(1))
 	})
 	t.Run("wrong type", func(t *testing.T) {
 		t.Parallel()
@@ -163,7 +165,7 @@ func TestUnmarshalInt(t *testing.T) {
 
 		var o Value[int]
 		g.Expect(o.UnmarshalJSON([]byte(`true`))).Error().To(HaveOccurred())
-		g.Expect(o.Present()).To(BeFalse())
+		g.Expect(o).To(opt.BeEmpty[int]())
 	})
 }
 
@@ -183,7 +185,7 @@ func TestGoStringFull(t *testing.T) {
 	})
 }
 
-func TestStringizeFull(t *testing.T) {
+func TestStringize(t *testing.T) {
 	t.Parallel()
 
 	t.Run("int", func(t *testing.T) {
@@ -200,36 +202,38 @@ func TestStringizeFull(t *testing.T) {
 		o := New("foo")
 		g.Expect(fmt.Sprintf("%v", o)).To(Equal(`foo`))
 	})
+	t.Run("empty", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
+
+		var o Value[int]
+		g.Expect(fmt.Sprintf("%v", o)).To(Equal(`None`))
+	})
 }
 
-func TestStringizeEmpty(t *testing.T) {
+func TestTransform(t *testing.T) {
 	t.Parallel()
-	g := NewWithT(t)
+	t.Run("same type", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
 
-	var o Value[int]
-	g.Expect(fmt.Sprintf("%v", o)).To(Equal(`None`))
-}
+		o := New(4)
+		g.Expect(Transform(o, func(i int) int { return i + 1 })).
+			To(opt.HaveValue(5))
+	})
+	t.Run("different type", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
 
-func TestTransformSameType(t *testing.T) {
-	t.Parallel()
-	g := NewWithT(t)
+		o := New(4)
+		g.Expect(Transform(o, func(i int) string { return fmt.Sprintf("%v", i) })).
+			To(opt.HaveValue("4"))
+	})
+	t.Run("empty", func(t *testing.T) {
+		t.Parallel()
+		g := NewWithT(t)
 
-	o := New(4)
-	g.Expect(Transform(o, func(i int) int { return i + 1 })).To(opt.HaveValue(5))
-}
-
-func TestTransformDifferentType(t *testing.T) {
-	t.Parallel()
-	g := NewWithT(t)
-
-	o := New(4)
-	g.Expect(Transform(o, func(i int) string { return fmt.Sprintf("%v", i) })).To(opt.HaveValue("4"))
-}
-
-func TestTransformEmpty(t *testing.T) {
-	t.Parallel()
-	g := NewWithT(t)
-
-	var o Value[int]
-	g.Expect(Transform(o, func(i int) int { return i + 1 })).To(opt.BeEmpty[int]())
+		var o Value[int]
+		g.Expect(Transform(o, func(i int) int { return i + 1 })).To(opt.BeEmpty[int]())
+	})
 }
